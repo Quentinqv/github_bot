5d36bfb403997e90f8f79c0c0a8685f0ca4e070b
Quentinqv <quentin010801@gmail.com>
2022-02-17 12:26:46 +0100

    Fin du bot

--- bot/index.js
+++ bot/index.js
@@ -1,6 +1,7 @@
 class IgBot {
   constructor() {
     this.config = require("./config/config.json")
+    this.creds = require("../creds")
   }
 
   /**
@@ -8,7 +9,6 @@ class IgBot {
    */
   async init() {
     const puppeteer = require("puppeteer")
-    const creds = require("../creds")
     this.browser = await puppeteer.launch(this.config.settings)
 
     // Ouverture d'un onglet
@@ -19,6 +19,64 @@ class IgBot {
 
     // On attend que le body soit chargé
     await this.page.waitForSelector("body")
+  }
+
+  /**
+   * Go to login page from "Sign in" button in header
+   * Fill login and password fields then submit form
+   */
+  async login() {
+    await this.page.waitForSelector(this.config.selectors.signin_button)
+    let signin_button = await this.page.$(this.config.selectors.signin_button)
+    await signin_button.evaluate((e) => e.click())
+
+    // Wait for login input and write it the username
+    await this.page.waitForSelector(this.config.selectors.login_field)
+    await this.page.focus(this.config.selectors.login_field)
+    await this.page.keyboard.type(this.creds.username)
+
+    // Wait for password input and write it the password
+    await this.page.waitForSelector(this.config.selectors.password_field)
+    await this.page.focus(this.config.selectors.password_field)
+    await this.page.keyboard.type(this.creds.password)
+
+    // Submit login form
+    await this.page.waitForSelector(this.config.selectors.login_submit)
+    await this.page.click(this.config.selectors.login_submit)
+  }
+
+  /**
+   * Go to search page with config.name on page 1 to (config.nbOfPage + 1)
+   */
+  async searchFor() {
+    for (let i = 1; i < this.config.nbOfPage + 1; i++) {
+      await this.page.goto(
+        `https://github.com/search?q=${this.config.name}&type=users&p=${i}`
+      )
+
+      const users = await this.page.$$(this.config.selectors.allusers)
+      console.log("nb of users", users.length)
+      for (const key in users) {
+        if (Object.hasOwnProperty.call(users, key)) {
+          const user = users[key]
+
+          const username = await user.evaluate((e) => {
+            let href = e.querySelector("a").href
+            href = href.split("/")
+            return href[href.length - 1]
+          })
+
+          const description = await user.evaluate((e) => {
+            let p = e.querySelector("p")
+            return p != null ? p.innerText : "No description"
+          })
+
+          console.log("Username :", username, "Description :", description)
+        }
+      }
+
+      await this.page.waitForTimeout(2000)
+    }
   }
 
   /**
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const Bot = require("./bot");
+const Bot = require("./bot")
 
 const run = async () => {
-  const bot = new Bot();
-  await bot.init().then(() => console.log("Init"));
-};
+  const bot = new Bot()
+  await bot.init().then(() => console.log("Init"))
+  await bot.login().then(() => console.log("Login in"))
+  await bot.searchFor().then(() => console.log("Ends of search"))
+  await bot.end().then(() => console.log("Ends of bot"))
+}
 
-run();
+run()
